def596aef33c1cf8ad2afc0f72f5a0a3b8bb9068
yh794406573 <794406573@qq.com>
2018-11-21 16:03:00 +0800

    Update note.py

--- note.py
+++ note.py
@@ -1,3 +1,6 @@
 #docker 构建镜像
 https://yeasy.gitbooks.io/docker_practice/image/build.html
 https://blog.csdn.net/YuZhiHui_No1/article/details/41129985
+#分布式集群
+分布式：一个业务分拆多个子业务，部署在不同的服务器上
+集群：同一个业务，部署在多个服务器上
